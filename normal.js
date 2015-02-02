599c69e5970a1094d738f93fffcb3b217a82704a
Becky Hamm <becky@metricslice.com>
2015-02-02 14:01:58 -0500

    initial commit

---
+++ normal.js
@@ -0,0 +1,81 @@
+var name;
+
+// Q: What is the value of name?
+// A: undefined
+
+
+name = "JD";
+
+// Q: What is the value of name?
+// A: "JD"
+
+
+name = "Toni"
+
+// Q: What is the value of name?
+// A: "Toni"
+
+
+var a = 5;
+
+// Q: What is the value of a?
+// A: 5
+
+
+var b = 10;
+
+// Q: What is the value of b?
+// A: 10
+
+
+a = b;
+
+// Q: What is the value of a?
+// A: 10
+
+
+b = 20;
+
+// Q: What is the value of a?
+// A: 10
+
+
+var greeting = "Hello " + name + ", how are you?";
+
+// Q: What is the value of greeting?
+// A: "Hello Toni, how are you?"
+
+
+var high = 50;
+var low = 10;
+
+var delta = high - low;
+
+// Q: What is the value of delta?
+// A: 40
+
+
+
+var math = delta + 20 - high;
+
+// Q: What is the value of math?
+// A: 40 + 20 - 50 = 10
+
+
+
+var givenName = "JD";
+var surName = "Isaacks";
+
+// Using the variables above create another variable
+// with the content "My Instructor's name is JD Isaacks"
+// A: var sentence = "My Instructor's name is " + givenName + " " + surName;
+
+
+
+var birthYear = 1984;
+var lastBirthday = 2014;
+
+// Using the varibles above create a new variable that
+// holds the value that is my age (30)
+// A: var age = lastBirthday - birthYear;
+
